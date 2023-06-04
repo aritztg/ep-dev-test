caaad9666023a7b85d1f81869765ea01e2322ea1
Aritz Tusell <aritz.tusell@otainsight.com>
2023-06-05 01:52:10 +0200

    Added some docstring

--- app/utils.py
+++ app/utils.py
@@ -2,9 +2,11 @@ import numpy as np
 
 
 def normalize(band):
+    """Normalize min/max values on a given band."""
     band_min, band_max = (band.min(), band.max())
     return ((band-band_min)/((band_max - band_min)))
 
 def brighten(band):
+    """Increase brightness on a given band, hardcoded values."""
     alpha=0.1
     return np.clip(alpha*band, 0, 255)
